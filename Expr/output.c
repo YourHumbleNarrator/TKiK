82c8565ca81cdec1062c735464bc339e92a7eb36
Mateusz Bielowka <mbielowka@student.agh.edu.pl>
2025-05-22 10:55:24 +0200

    Compiler work 5

--- Expr/output.c
+++ Expr/output.c
@@ -1,26 +1,47 @@
 void quicksort(int[False] array, int inizio1, int fine1); {
-        if (wip) {
-            ['int pivot = array[fin];', 'int indice = inizio1-1;', '//TODO165', 'scambia(array[indice+1], array[fin]);', 'int partizione = indice+1;', 'quicksort(array, inizio1, partizione-1);', 'quicksort(array, partizione+1, fine1);']
-            }
-        }
+
+if (wip) {
+
+int pivot = array[fin];
+int indice = inizio1-1;
+for (int i = inizio1; i <= fine1-1; i++) {
+if (wip) {
+
+indice = indice+1;
+scambia(array[indice], array[i]);
+}
+}
+scambia(array[indice+1], array[fin]);
+int partizione = indice+1;
+quicksort(array, inizio1, partizione-1);
+quicksort(array, partizione+1, fine1);
+}
+}
 
 void scambia(int a, int b); {
-        int temp = a;
+
+int temp = a;
 a = b;
 b = temp;
-        }
+}
 
 int main() {
-    int array[100];
+
+int array[100];
 int n;
 bool temp = 1;
 bool temp2 = wip;
 //TODO4
 //TODO5
 //TODO4
-//TODO165
+for (int i = 0; i <= n-1; i++) {
+//TODO4
+}
 quicksort(array, 0, n-1);
 //TODO4
-//TODO165
-    return 0;
-    }
+for (int i = 0; i <= n-1; i++) {
+//TODO4
+//TODO4
+}
+return 0;
+}
