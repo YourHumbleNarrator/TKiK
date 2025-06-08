fff42a3bf96f78324db5587167c98eb1e87173c2
berenike <berenike@student.agh.edu.pl>
2025-06-08 18:20:31 +0200

    quick fixes in error handling

--- Expr/ErrorHandling.py
+++ Expr/ErrorHandling.py
@@ -42,11 +42,11 @@ class ErrorHandling(ExprVisitor):
         if ctx.return_statement():
             if not self.return_type:
                 line = ctx.return_statement().symbol.line
-                self.errors.append(f"[line {line}]This function should not return anything.")
+                self.errors.append(f"This function should not return anything (line {line})")
         else:
             if self.return_type:
                 line = ctx.return_statement().symbol.line
-                self.errors.append(f"[line {line}]This function should return a value.")
+                self.errors.append(f"This function should return a value (line {line})")
         if ctx.local_variable_declaration():
             self.visit(ctx.local_variable_declaration())
 
@@ -54,7 +54,7 @@ class ErrorHandling(ExprVisitor):
         var_name = ctx.lvalue()
         if var_name in self.variables:
             line = ctx.lvalue().symbol.line
-            self.errors.append(f"[line {line}]Variable '{var_name}' has been declared multiple times.")
+            self.errors.append(f"Variable '{var_name}' has already been declared (line {line})")
         else:
             self.variables.add(var_name)
         if ctx.expression():
@@ -65,17 +65,17 @@ class ErrorHandling(ExprVisitor):
         var_name = ctx.IDENTIFIER().getText()
         if var_name not in self.variables:
             line = ctx.IDENTIFIER().symbol.line if ctx.IDENTIFIER() else ctx.start.line
-            self.errors.append(f"[line {line}]Variable '{var_name}' has been used before declaration.")
+            self.errors.append(f"Variable '{var_name}' has not been declared yet (line {line})")
 
     def visitFunction_call(self, ctx: ExprParser.Function_callContext):
         if ctx.expression():
             if self.number_of_arguments[ctx.IDENTIFIER().getText()] == 0:
                 line = ctx.expression().symbol.line
-                self.errors.append(f"[line {line}]This function should not have any parameters.")
+                self.errors.append(f"This function should not have any parameters (line {line})")
         else:
             if self.number_of_arguments[ctx.IDENTIFIER().getText()] == 1:
                 line = ctx.expression().symbol.line
-                self.errors.append(f"[line {line}]This function should have some parameters.")
+                self.errors.append(f"This function should have some parameters (line {line})")
 
     def visitType_specifier(self, ctx: ExprParser.Type_specifierContext):
         if ctx.BOOL_TP():
@@ -88,20 +88,20 @@ class ErrorHandling(ExprVisitor):
     def visitExpression(self, ctx: ExprParser.ExpressionContext):
         if ctx.math_expression() and self.type_specifier == "logical":
             line = ctx.math_expression().symbol.line
-            self.errors.append(f"[line {line}]You are trying to assign a number to a boolean variable.")
+            self.errors.append(f"You cannot assign a number to a boolean variable (line {line})")
         if ctx.math_expression() and self.type_specifier == "character":
             line = ctx.math_expression().symbol.line
-            self.errors.append(f"[line {line}]You are trying to assign a number to a char variable.")
+            self.errors.append(f"You cannot assign a number to a char variable (line {line})")
         if ctx.logical_expression() and self.type_specifier == "mathematical":
             line = ctx.math_expression().symbol.line
-            self.errors.append(f"[line {line}]You are trying to assign a boolean value to a number variable.")
+            self.errors.append(f"You cannot assign a boolean value to a number variable (line {line})")
         if ctx.logical_expression() and self.type_specifier == "character":
             line = ctx.math_expression().symbol.line
-            self.errors.append(f"[line {line}]You are trying to assign a boolean value to a char variable.")
+            self.errors.append(f"You cannot assign a boolean value to a char variable (line {line})")
         if ctx.CHAR_LITERAL() and self.type_specifier == "mathematical":
             line = ctx.math_expression().symbol.line
-            self.errors.append(f"[line {line}]You are trying to assign a character to a number variable.")
+            self.errors.append(f"You cannot assign a character to a number variable (line {line})")
         if ctx.CHAR_LITERAL() and self.type_specifier == "logical":
             line = ctx.math_expression().symbol.line
-            self.errors.append(f"[line {line}]You are trying to assign a character to a boolean variable.")
+            self.errors.append(f"You cannot assign a character to a boolean variable (line {line})")
 
